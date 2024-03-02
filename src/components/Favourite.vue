ca7d3d8145cc41859cd240a3e116c62730527fc9
Oleg Kozina <olegkozina@Olegs-MacBook-Pro.local>
2024-03-01 19:24:51 -0700

    formatting with prettier

--- src/components/Favourite.vue
+++ src/components/Favourite.vue
@@ -1,50 +1,44 @@
 <template>
-
-        <h2>Favourite options</h2>
-        <div @click="getFromFavs(index)" v-for="(el, index) in favs" :key="index">
-            <span>{{ el.from }}</span>
-            <span>{{ el.to }}</span>
-        </div>
-
-
+  <h2>Favourite options</h2>
+  <div @click="getFromFavs(index)" v-for="(el, index) in favs" :key="index">
+    <span>{{ el.from }}</span>
+    <span>{{ el.to }}</span>
+  </div>
 </template>
 <script>
-
 export default {
-
-props:{
-    favs:{
-        type: Array,
-        required: true
+  props: {
+    favs: {
+      type: Array,
+      required: true,
     },
-    getFromFavs:{
-        type: Function,
-        required: true
-    }
-}}
-
+    getFromFavs: {
+      type: Function,
+      required: true,
+    },
+  },
+};
 </script>
 <style scoped>
-h2{
-    color: white;
+h2 {
+  color: white;
 }
-div{
-    display: flex;
-    justify-content: space-between;
-    width: 350px;
-    margin: 0 auto;
+div {
+  display: flex;
+  justify-content: space-between;
+  width: 350px;
+  margin: 0 auto;
+}
 
+span {
+  padding: 10px 15px;
+  background: purple;
+  color: white;
+  transition: all 500ms ease;
 }
-
-span{
-    padding: 10px 15px;
-    background: purple;
-    color: white;
-    transition: all 500ms ease;
-}
-span:hover{
-    opacity: 0.7;
-    transform: scale(1.1);
-    cursor: pointer;
+span:hover {
+  opacity: 0.7;
+  transform: scale(1.1);
+  cursor: pointer;
 }
 </style>
--- src/components/Selector.vue
+++ src/components/Selector.vue
@@ -1,49 +1,58 @@
 <template>
-
-        <ul>
-            <li @click="setCrypto('BTC')" :className="cryptoNow == 'BTC' ? 'active' : ''">Bitcoin</li>
-            <li @click="setCrypto('ETH')" :className="cryptoNow == 'ETH' ? 'active' : ''">ETH</li>
-            <li @click="setCrypto('USDT')" :className="cryptoNow == 'USDT' ? 'active' : ''">USDT</li>
-        </ul>
-
- 
-
+  <ul>
+    <li
+      @click="setCrypto('BTC')"
+      :className="cryptoNow == 'BTC' ? 'active' : ''"
+    >
+      Bitcoin
+    </li>
+    <li
+      @click="setCrypto('ETH')"
+      :className="cryptoNow == 'ETH' ? 'active' : ''"
+    >
+      ETH
+    </li>
+    <li
+      @click="setCrypto('USDT')"
+      :className="cryptoNow == 'USDT' ? 'active' : ''"
+    >
+      USDT
+    </li>
+  </ul>
 </template>
 
 <script>
-
-
 export default {
-   props: {
+  props: {
     setCrypto: {
-    type: Function,
-    required: true,
-   },
-   cryptoNow:{
-    type: String,
-    required: true
-   }},
- 
-}
+      type: Function,
+      required: true,
+    },
+    cryptoNow: {
+      type: String,
+      required: true,
+    },
+  },
+};
 </script>
 
 <style scoped>
-
-ul{
-    list-style: none;
-    width: 300px;
-    border: 3px solid #000;
-    border-radius: 5px;
-    padding: 0;
-    align-content: center;
+ul {
+  list-style: none;
+  width: 300px;
+  border: 3px solid #000;
+  border-radius: 5px;
+  padding: 0;
+  align-content: center;
 }
-li{
-    display: block;
-    color: #fff;
-    background:  rgba(102, 47, 47, 0.56);
+li {
+  display: block;
+  color: #fff;
+  background: rgba(102, 47, 47, 0.56);
 }
-li:hover, .active {
-    cursor: pointer;
-    background:#000;
+li:hover,
+.active {
+  cursor: pointer;
+  background: #000;
 }
 </style>
